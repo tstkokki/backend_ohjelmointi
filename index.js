3d502b9b78e5d17bfafecd477bfac0040429d400
tstkokki <60537602+tstkokki@users.noreply.github.com>
2020-11-03 16:28:48 +0200

    cookies and sessions

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const url = 'mongodb://localhost:27017/messagedb';
 
+
+
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -20,11 +22,76 @@ const options = {
     family: 4 // use IPv4, skip IPv6
 };
 
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 var connect = mongoose.connect(url, options);
 
 //create express app
 var app = express();
 app.use(morgan('dev'));
+
+// use cookies
+app.use(cookieParser());
+//use session with secret key
+app.use(session(
+    {secret:'Very very secret!',
+    resave: false,
+    saveUninitialized: false
+    }));
+
+//basic authentication
+function auth(req, res, next){
+    console.log(req.headers);
+    console.log('Session data: ', req.session);
+    if(req.session.user){
+        if(req.session.user == 'admin'){
+            next();
+        }
+    } else{
+
+        var authHeader = req.headers.authorization;
+        if(authHeader == null){
+            console.log("No authorization header found");
+            res.setHeader('WWW-Authenticate', 'Basic');
+            var err = new Error('Not authenticated!');
+            err.status = 401;
+            next(err);
+        } else {
+            //process the authorization header info
+            //split
+            var auth_string = authHeader.split(' ')[1];
+            //decode (base64)
+            var decoded_auth = new Buffer.from(auth_string, 'base64').toString();
+            //get username and password
+            let username = decoded_auth.split(':')[0];
+            let password = decoded_auth.split(':')[1];
+            //check username and password
+            if (username == 'Naapuri' && password == 'Pirkko'){
+                req.session.user = 'admin';
+                // proceed
+                next();
+            } else {
+                console.log("Wrong username and/or password");
+                // if(req.cookies.login_attempts){
+                //     //get biscuits and add one
+                //     let attempts = parseInt(req.cookies.login_attempts);
+                //     attempts++;
+                //     res.cookie('login_attempts', attempts);
+                // } else {
+                //     res.cookie('login_attempts', '1');
+                // }
+                res.setHeader('Authenticate', 'Basic');
+                var err = new Error('Not Authenticated!');
+                err.status = 401;
+                next(err);
+            }
+        }   
+    }
+}
+//use the authentication
+app.use(auth);
 
 //get the model for messages
 //const Messages = require('./models/messages');
